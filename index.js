d7d2e7f36285a802a9bc006d9e7b9349206bfb83
Addomike <addomichaelopare@gmail.com>
2026-01-08 01:57:14 +0000

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 ======================= */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
+
 
 /* =======================
    CORS Configuration
