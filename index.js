263ac94b42ccaf93ac81a36be07c06a14eaa2ca4
Addomike <addomichaelopare@gmail.com>
2026-01-08 10:01:26 +0000

    first commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
    CORS Configuration
 ======================= */
 const allowedOrigins = [
-  "https://nayaaxisfoods.vercel.app"
+  "https://nayasuccessaxis.com"
 ];
 
 app.use(
