dabb51ba80105f25d8ed3644be536aa7f1f5e088
Addomike <addomichaelopare@gmail.com>
2026-01-08 10:08:47 +0000

    first commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
    CORS Configuration
 ======================= */
 const allowedOrigins = [
-  "https://nayasuccessaxis.com"
+  "https://www.nayasuccessaxis.com"
 ];
 
 app.use(
