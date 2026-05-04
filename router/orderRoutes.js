69a3884ebfa4b4265da200fa124b6c346615b532
Addomike23 <addomichaelopare@gmail.com>
2026-05-04 16:20:48 +0000

    aa

--- router/orderRoutes.js
+++ router/orderRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const orderRouter = express.Router();
 const { createOrder, getOrders, deleteAllOrders, getOrderByNumber } = require("../controller/orderItemController");
-
+orderRouter.use(express.json());
 
 orderRouter.post('/create', createOrder);
 orderRouter.get('/all', getOrders);
-orderRouter.get('/number/:orderNumber', getOrderByNumber);  // ✅ Add this route
+orderRouter.get('/number/:orderNumber', getOrderByNumber);  
 orderRouter.delete('/delete-all', deleteAllOrders);
 
 
